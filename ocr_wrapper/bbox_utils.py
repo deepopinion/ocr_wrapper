7e75d5ceae581c6dd209291fe6202bfa8c393575
Sebastian Stabinger <sebastian@stabinger.name>
2023-05-11 15:44:14 +0200

    Improve bbox_intersection_area_percent performance by adding lru_cache and handling non-intersecting cases

--- ocr_wrapper/bbox_utils.py
+++ ocr_wrapper/bbox_utils.py
@@ -1,9 +1,12 @@
 """
 Additional functions for working with BBox objects.
 """
+from functools import lru_cache
+
 from .bbox import BBox
 
 
+@lru_cache
 def bbox_intersection_area_percent(bb1: BBox, bb2: BBox) -> float:
     """Returns the area of the intersection of BBox `bb1` with BBox `bb2` as a proportion of the area of `bb1`.
 
@@ -11,5 +14,8 @@ def bbox_intersection_area_percent(bb1: BBox, bb2: BBox) -> float:
     """
     self_poly = bb1.get_shapely_polygon()
     that_poly = bb2.get_shapely_polygon()
-    inter_poly = self_poly.intersection(that_poly)
-    return inter_poly.area / self_poly.area
+    if self_poly.intersects(that_poly):
+        inter_poly = self_poly.intersection(that_poly)
+        return inter_poly.area / self_poly.area
+    else:
+        return 0.0

2262d2e7a46b8041881f8e7d69709ac9a492ec2f
Sebastian Stabinger <sebastian@stabinger.name>
2023-11-09 16:01:48 +0100

    Moves import of autoselect_ocr_engine to end due to dependency on other modules
    Otherwise we get a circular import error

--- ocr_wrapper/__init__.py
+++ ocr_wrapper/__init__.py
@@ -1,4 +1,3 @@
-from .autoselect import autoselect_ocr_engine
 from .aws import AwsOCR
 from .azure import AzureOCR
 from .bbox import BBox, draw_bboxes, get_label2color_dict
@@ -6,6 +5,9 @@ from .easy_ocr import EasyOCR
 from .google_ocr import GoogleOCR
 from .ocr_wrapper import OcrWrapper
 from .paddleocr import PaddleOCR
+
+# Important as last import, because it depends on the other modules
+from .autoselect import autoselect_ocr_engine  # isort:skip
 
 __all__ = [
     "AwsOCR",

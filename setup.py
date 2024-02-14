9930c5378739cfcd1c716c8263e2d7fba7ee6046
Sebastian Stabinger <sebastian@stabinger.name>
2024-02-14 11:13:39 +0100

    Specify Shapely version range in setup.py dependencies

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     ],
     install_requires=[
         "Pillow",
-        "Shapely",
+        "Shapely>=2.0,<3.0",
         "pdf2image",
         "rtree",
         "opencv-python-headless",
